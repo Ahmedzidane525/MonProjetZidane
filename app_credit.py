a29e88a095b57737eb43e0e4a9212eb54e50e4df
Ahmedzidane525 <zidane.ahmed525@gmail.com>
2025-02-10 01:38:06 +0100

    Update app_credit.py

--- app_credit.py
+++ app_credit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import seaborn as sns 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
